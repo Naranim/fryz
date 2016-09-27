f76e55192738fad38d75cbd7825a7652ef29ff10
Bartlomiej Szczepanik <mequrel@gmail.com>
2016-09-27 23:18:50 +0200

    changed grunt:serve port to allow mobile testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,8 +71,7 @@ module.exports = function (grunt) {
     connect: {
       options: {
         port: 9000,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
@@ -224,7 +223,7 @@ module.exports = function (grunt) {
         src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
         ignorePath: /(\.\.\/){1,2}bower_components\//
       }
-    }, 
+    },
 
     // Compiles Sass to CSS and generates necessary files if requested
     compass: {
